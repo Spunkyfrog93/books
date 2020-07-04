8dbb5f4f54776a742972e56ae89b7b1e16aac11c
Spunkyfrog93 <60330400+Spunkyfrog93@users.noreply.github.com>
2020-07-04 13:57:32 -0400

    style changes

--- google-books/client/src/components/Nav/index.js
+++ google-books/client/src/components/Nav/index.js
@@ -1,13 +1,13 @@
 import React from "react";
 
 function Nav() {
-  return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-      <a className="navbar-brand" href="/">
-        Book List
-      </a>
-    </nav>
-  );
+    return (
+        <nav className="navbar navbar-expand-lg navbar-primary bg-primary">
+            <a className="navbar-brand" href="/">
+                <h2 className="text-white">Google Book Search</h2>
+            </a>
+        </nav>
+    );
 }
 
 export default Nav;
